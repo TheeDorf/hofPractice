678131b68cec37705f0944bc01a63f08c63cb984
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-20 08:21:14 -0700

    practice

---
+++ index.js
@@ -0,0 +1,69 @@
+let nums = [1, 2, 3, 4, 5];
+let arr1 = [20,40,60,80,3];
+
+const pokemon = [
+    {name: "pikachu",
+    type: "lightning",
+    damage: 20,
+    },
+    {name: "machamp",
+    type: "fighting",
+    damage:60,
+        },
+    {name: "mew",
+    type: "psychic",
+     damage:30,
+            },
+    {name: "charizard",
+    type: "fire",
+    damage: 100,
+                },
+];
+pokemon.forEach ((pokemon)=>{
+    console.log(pokemon.name, pokemon.damage);
+})         
+
+// nums.reduce((sum, num) => {
+//   return sum + num;
+// }, 0);
+
+// console.log(
+//     arr1.reduce((sum,num)=>{
+//         if(num % 10 !==0){
+//             console.log(num);
+//         }
+//         else{
+//             console.log(num);
+//             return sum+= num;
+//         }
+//         return sum
+//     },0 )
+// );
+
+// var sum = 0;
+// arr1.forEach((num)=>{
+//     if(num % 10 !==0){
+//         console.log("in the if" + num);
+//     }
+//     else{
+//         // console.log(num);
+//         return sum+= num;
+//     }
+//     console.log(sum)
+//     return sum;
+
+// })
+
+
+// arr.reduce(function(previousValue, currentValue, currentIndex, array){
+//     previousValue += currentValue;
+// }, initialValue);
+
+// arr.reduce((total,currentValue, currentIndex, array)=>{
+//     total+=currentValue;
+// }, initialValue)''
+
+
+nums.reduce(function red(){
+
+})
